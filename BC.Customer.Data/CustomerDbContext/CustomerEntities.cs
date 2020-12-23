1744bb7f0e3fc51388bc997ce4272dd04bb59b10
Nandagoban Subramaniam <30495008+nandagoban08@users.noreply.github.com>
2020-12-23 14:43:22 +0800

    Update CustomerEntities.cs

--- BC.Customer.Data/CustomerDbContext/CustomerEntities.cs
+++ BC.Customer.Data/CustomerDbContext/CustomerEntities.cs
@@ -22,11 +22,6 @@ namespace BC.Customer.Data.CustomerDbContext
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-//            if (!optionsBuilder.IsConfigured)
-//            {
-//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-//                optionsBuilder.UseSqlServer("Server=igisystem.database.windows.net,1433;Database=bookcabin;User ID=bcadmin;Password=Pass#word1@;Trusted_Connection=False;");
-//            }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
